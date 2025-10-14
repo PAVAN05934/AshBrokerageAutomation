de8740678b0ede47723eaeea51ca34f62962e228
Pavan <pavank.itpath@gmail.com>
2025-10-14 14:14:57 +0530

    Upto Quote Result

--- src/test/java/AshBrokerage/main/Beneficiary_Info_page.java
+++ src/test/java/AshBrokerage/main/Beneficiary_Info_page.java
@@ -1,5 +1,17 @@
 package AshBrokerage.main;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Beneficiary_Info_page {
+	
+	public WebDriver driver;
+	
+	public Beneficiary_Info_page(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
 
 }
